19b98595d537668d00fb9e22358efb69a8e3f034
IsaqueGarcia <isaqueemprego1@gmail.com>
2019-08-29 22:46:35 -0300

    Projeto Criado

---
+++ src/main/java/com/rest/api/TreinamentoAPI/TreinamentoApiApplication.java
@@ -0,0 +1,13 @@
+package com.rest.api.TreinamentoAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreinamentoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreinamentoApiApplication.class, args);
+	}
+
+}
